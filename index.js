e9d19bc7c647a0789adca3a0ede44fc92885c4b8
Abdulaziz <abdulaziz.komara@student.moringaschool.com>
2023-06-21 19:27:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// Write your code here!
+let message;
+
+function scuberGreetingForFeet(distance){
+  if (distance<=400){
+  message="This one is on me!"
+} else if (distance>2000 && distance<2500){
+  message="I will gladly take your thirty bucks."
+} else if(distance>2500){
+  message="No can do."
+}return message;
+}
+scuberGreetingForFeet(199);
+
+
+function ternaryCheckCity(city){
+if(city==='NYC'){
+message='Ok,sounds good.'
+}
+ else {
+  message='No,go.'
+
+}return message;
+}
+function ternaryCheckCity(city){
+  if(city ==='NYC') {
+    return 'Ok, sounds good.'
+  }else {
+    return 'No go.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."
+      break;
+  }
 }
